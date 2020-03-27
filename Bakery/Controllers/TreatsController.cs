5573e3ee6cd4f71cc6be2f735d109d7944c0dd78
acolistro <arcolistro@gmail.com>
2020-03-27 14:38:30 -0700

    refactor some views and treats controller to resolve authentication issue with treats and display both treats and flavors on index.

--- Bakery/Controllers/TreatsController.cs
+++ Bakery/Controllers/TreatsController.cs
@@ -37,7 +37,6 @@ namespace Bakery.Controllers
       return View();
     }
 
-    //updated Create post method
     [HttpPost]
     public async Task<ActionResult> Create(Treat treat, int FlavorId)
     {
